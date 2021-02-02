7350f7d0e9b7d2a3804f15ef108ca22bd52f1ad8
Taina Batista Conde <tainaconde@Tainas-MBP.localdomain>
2021-02-02 15:29:54 -0600

    feat: Create setAuthedUser action
    Import setAuthedUser action to shared action file.
    Dispatch that action in handleInitialData.
    TODO: authentication process coming from sign in.

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,12 @@
 import { getInitialData } from '../utils/api'
 import { receiveQuestions } from './questions'
 import { receiveUsers } from './users'
+import { setAuthedUser } from './authedUser'
+
+
+// TODO: authentication process
+const AUTHED_ID = "" // Coming from sign in 
+
 
 export function handleInitialData() {
     return (dispatch) => {
@@ -8,6 +14,8 @@ export function handleInitialData() {
             .then (({questions, users}) => {
                 dispatch(receiveQuestions(questions))
                 dispatch(receiveUsers(users))
+                dispatch(setAuthedUser(AUTHED_ID))
+
             })
     }
 }
