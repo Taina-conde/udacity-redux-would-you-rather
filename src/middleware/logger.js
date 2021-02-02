1de250038a544d1fd06c8d4874d75a144b0bdcbe
Taina Batista Conde <tainaconde@Tainas-MBP.localdomain>
2021-02-02 17:04:42 -0600

    feat: Logger middleware

---
+++ src/middleware/logger.js
@@ -0,0 +1,8 @@
+export default const logger = (store) => (next) => (action) => {
+    console.group(action.type)
+        console.log('Action: ', action)
+        const returnedValue = next(action)
+        console.log('New state: ', store.getState())
+    console.groupEnd
+    return returnedValue
+} 
