91788420f6f5796184c90e40ba3a3653457108cb
Taina Batista Conde <tainaconde@Tainas-MBP.localdomain>
2021-02-02 16:01:48 -0600

    fix: AuthedUser reducer
    In case SET_AUTHED_USER, return action.id.

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -3,10 +3,7 @@ import {SET_AUTHED_USER} from '../actions/authedUser'
 export default function authedUserReducer (state = null, action) {
     switch(action.type) {
         case SET_AUTHED_USER:
-            return {
-                ...state,
-                action.authedUser
-            }
+            return action.id
         default :
             return state
     }
