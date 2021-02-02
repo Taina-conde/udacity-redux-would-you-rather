6083700f9af06848bca0cbe7b86813bfbc7e50b8
Taina Batista Conde <tainaconde@Tainas-MBP.localdomain>
2021-02-02 15:57:09 -0600

    feat: Combine Reducers and rename shared file
    Install redux library. Rename shared.js to index.js in reducers folder.
    Import combineReducers and use it to combine all the reducers into one.

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { usersReducer } from './users'
+import { questionsReducer } from './questions'
+import { authedUserReducer } from './authedUser'
+import { combineReducers } from 'redux'
+
+export default combineReducers({
+    users: usersReducer,
+    questions: questionsReducer,
+    authedUser : authedUserReducer
+})
