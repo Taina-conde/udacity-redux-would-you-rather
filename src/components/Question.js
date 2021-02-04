0db2eafb95b0065251efad56b3fdd3080f654f34
Taina Batista Conde <tainaconde@Tainas-MBP.localdomain>
2021-02-04 16:40:54 -0600

    feat: Question - UI
    Add UI for question author asks and author's avatar.

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,17 +3,30 @@ import { connect } from 'react-redux';
 
 class Question extends React.Component {
     render(){
+        const {authedUser, user, question} = this.props;
         return (
             <div>
+                <div>
+                    <h4>{`${question.author} asks:`}</h4>
+                </div>
+                <div>
+                    <div className = "user-avatar">
+                        <img src = {users[question.author].avatar.avatarURL} alt = {`avatar-${question.author}`}/>
+                    </div>
+                    <div className = "question-content">
+
+                    </div>
+                </div>
 
             </div>
         )
     }
 }
 
-function mapStateToProps({questions, authedUser}, {id}) {
+function mapStateToProps({questions, authedUser users}, {id}) {
     return {
         authedUser,
+        users,
         question: questions[id]
     }
 }
