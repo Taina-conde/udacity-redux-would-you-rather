70d88d8fcbe77311367f42b4eef2f66ab463c537
Taina Batista Conde <tainaconde@Tainas-MBP.localdomain>
2021-02-04 16:02:48 -0600

    feat: Question - create component's skeleton

---
+++ src/components/Question.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class Question extends React.Component {
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+export default connect()(Question);
