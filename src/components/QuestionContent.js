30687d0b4e2b8cc275e6b04ecabe2e3918bf1e4d
Taina Batista Conde <tainaconde@Tainas-MBP.localdomain>
2021-02-04 18:46:48 -0600

    feat: QuestionContent - component skeleton
    Create QuestionContent connected to store because it will need
    access to questions to know if it is answered or unanswered and
    the options. It will need to acces authedUser to know his answer
    and users as well.

---
+++ src/components/QuestionContent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+class QuestionContent extends React.Component {
+    render() {
+        return ()
+    }
+}
+export default connect()(QuestionContent);
