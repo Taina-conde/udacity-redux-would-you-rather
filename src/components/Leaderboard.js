18ddbfad3e8d39f211703be84a489bfe6c2c1c7c
Taina Conde <tainabatistaconde@gmail.com>
2021-02-08 16:29:38 -0600

    feat: Learderboard - UI draft

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -5,9 +5,25 @@ class Leaderboard extends React.Component {
     render() {
         return (
             <div>
+                <div>
+                    <div>
+                        <img></img>
+                    </div>
+                    <div>
+                        <h1>User name</h1>
+                    </div>
+                    <div>
+                        Answered questions:
+                    </div>
+                    <div>
+                        Created questions: 
+                    </div>
+                    <div>
+                        Score
+                    </div>
+                </div>
 
             </div>
-
         )
     }
 }
