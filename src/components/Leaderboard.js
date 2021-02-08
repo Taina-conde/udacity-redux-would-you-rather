e5051fd413b5acd7cf83642168b539a10acef567
Taina Conde <tainabatistaconde@gmail.com>
2021-02-08 16:22:58 -0600

    feat: Leaderboard - connect to store
    Import connect from react-redux. Connect component to store in order to
    access the users part of state through mapStateToProps.

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
 class Leaderboard extends React.Component {
     render() {
         return (
-            <div></div>
+            <div>
+
+            </div>
 
         )
     }
 }
-export default Leaderboard;
+function mapStateToProps({users}) {
+    return {
+        users
+    }
+}
+export default connect(mapStateToProps)(Leaderboard);
