c3391678493f11b86eb69bc626e709ec3f8e0961
Taina Conde <tainabatistaconde@gmail.com>
2021-02-09 17:34:49 -0600

    feat: Create LogInfo component

---
+++ src/components/LogInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class LogInfo extends React.Component {
+    render() {
+        return (
+            <div></div>
+
+        )
+    }
+}
+
+export default connect()(LogInfo);
