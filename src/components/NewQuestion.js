b147a96ed00df35af6cba55c11efac04e23030f4
Taina Conde <tainabatistaconde@gmail.com>
2021-02-09 10:31:03 -0600

    feat: NewQuestion - UI draft

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -4,10 +4,34 @@ import { connect } from 'react-redux';
 
 class NewQuestion extends React.Component {
     render() {
-        return( 
-        <div>
+        return ( 
+            <div>
+                <div>
+                    <h1>Create new question</h1>
+                </div>
+                <div>
+                    <form>
+                        <p> Complete the question:</p>
+                        <h2>Would you rather...</h2>
+                        <input 
+                            type= 'text' 
+                            placeholder= 'Enter option one text here'
+                        />
+                        <br/>
+                        <p>OR</p>
+                        <br/>
+                        <input
+                            type= 'text'
+                            placeholder = 'Enter option two text here'
+                        />
+                        
+                    </form>
+                    
+                    
+                </div>
 
-        </div>)
+            </div>
+        )
     }
 }
 export default connect()(NewQuestion);
