373401d5d4f32c75fdf1e386d17aecc5c1bbda6c
Taina Conde <tainabatistaconde@gmail.com>
2021-02-09 17:44:09 -0600

    feat: Loginfo- UI draft

--- src/components/LogInfo.js
+++ src/components/LogInfo.js
@@ -3,9 +3,17 @@ import { connect } from 'react-redux';
 
 class LogInfo extends React.Component {
     render() {
+        const { user } = this.props;
         return (
             <div>
-                <p>Hello, </p>
+                <p>Hello, {user.name} </p>
+                <img
+                    src= {user.avatar.avatarURL}
+                    alt = {`Avatar of ${user.id}`}
+                    style = {{height: "30px"}}
+                />
+                <button>Log out</button> 
+
             </div>
 
         )
