660fd492be57f0972616ef4afcc5a3db8058ee47
Taina Conde <tainabatistaconde@gmail.com>
2021-02-10 16:34:07 -0600

    feat: Create NotFound Component
    This component renders the 404 page that informs the user that the
    question he/she is looking for does not exist.

---
+++ src/components/NotFount.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NotFound () {
+    return (
+        <div> 
+            <h1>404</h1>
+            <p>Sorry, but it seems that the question you are looking for does not exist... </p>
+        </div>
+    )
+}
+export default NotFound;
