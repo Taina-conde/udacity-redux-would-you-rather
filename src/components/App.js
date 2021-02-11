dfcd5d1b4ad2d343cb030f6bb96f21c4b1117aa9
Taina Conde <tainabatistaconde@gmail.com>
2021-02-11 16:41:14 -0600

    style: Nav - collapse, navbar-nav, nav-item, nav-link

--- src/components/App.js
+++ src/components/App.js
@@ -23,8 +23,16 @@ class App extends React.Component {
         <React.Fragment>  
           <div className = 'container'>
             <nav className = 'navbar navbar-expand-lg'>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+              <button 
+                className="navbar-toggler" 
+                type="button" 
+                data-toggle="collapse" 
+                data-target="#navbarToggler" 
+                aria-controls="navbarToggler" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
               </button>
               <Nav/>
               <LogInfo/>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,24 +3,27 @@ import { NavLink } from 'react-router-dom'
 
 export default function Nav() {
     return (
-        <nav className = 'nav'>
-            <ul>
-                <li>
-                    <NavLink to = '/' exact activeClassName = 'active'>
+        <div 
+         className = "collapse navbar-collapse" 
+         id="navbarToggler"
+        >
+            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li className="nav-item">
+                    <NavLink className = 'nav-link' to = '/' exact activeClassName = 'active'>
                         Home
                     </NavLink>
                 </li>
-                <li>
-                    <NavLink to = '/add' activeClassName = 'active'>
+                <li className="nav-item">
+                    <NavLink className = 'nav-link' to = '/add' activeClassName = 'active'>
                         New question
                     </NavLink>
                 </li>
-                <li>
-                    <NavLink to = '/leaderboard' activeClassName = 'active'>
+                <li className="nav-item">
+                    <NavLink className = 'nav-link' to = '/leaderboard' activeClassName = 'active'>
                         Leader board
                     </NavLink>
                 </li>
             </ul>
-        </nav>
+        </div>
     )
 }
