36cef12c60022abe1a5c48b22172fa46c9de06b3
Taina Conde <tainabatistaconde@gmail.com>
2021-02-16 19:04:26 -0500

    style: LogInPage - replace divs for card

--- src/components/LonInPage.js
+++ src/components/LonInPage.js
@@ -35,84 +35,90 @@ class LogInPage extends React.Component {
             return <Redirect to = '/'/>
         }
         return (
-            <div className = 'container border rounded'>
-                <div className = 'row p-3 bg-light border-bottom mb-3'>
-                    <div>Let's play Would You Rather? Before we begin, sign in to continue.</div>
+            <div className = 'card'>
+                <div className = 'card-header text-center'>
+                    <h3 className = 'card-title'>Would you rather?</h3>
+                    <div className = 'card-text'>Let's play Would You Rather? Before we begin, sign in to continue.</div>
                 </div>
-                <div className = 'row'>
-                    <img
-                        className = 'img-fluid'
-                        src = {players}
-                        alt = 'players'
-                    />
-                </div>
-                <div className = 'row m-4'>
-                    <form  className = 'login-form' onSubmit = {this.handleSubmit}>
-                        <div className = 'form-group d-flex flex-column'>
-                            <label>Sign in</label>
-                            <InputGroup className = 'justify-content-center' size = 'lg'>
-                            
-                                <InputGroup.Prepend >
-                                    <InputGroup.Text  >
-                                        {this.state.value === "" 
-                                            ? "Select user ..." 
-                                            : 
-                                            <div className = 'd-flex flex-row align-items-center'>
-                                                <img 
-                                                    src ={users[this.state.value].avatar.avatarURL} 
-                                                    alt = {`Avatar of ${this.state.value}`}
-                                                    
-                                                    className = 'login-img mr-2'
-                                                    /> 
-                                                <div>{this.state.value}</div>
-                                            </div>
-                                        }
-                                    </InputGroup.Text>
+                <div className = 'card-body' >
+
+                <div className = 'border border-success card-content p-5'>
+                    <div className = 'text-center'>
+                        <img
+                            className = 'img-fluid'
+                            style = {{width: '50%'}}
+                            src = {players}
+                            alt = 'players'
+                        />
+                    </div>
+                    <div className = 'm-2'>
+                        <form  className = 'login-form' onSubmit = {this.handleSubmit}>
+                            <div className = 'form-group d-flex flex-column'>
+                                <label>Sign in</label>
+                                <InputGroup className = 'justify-content-center' size = 'lg'>
                                 
-                                </InputGroup.Prepend>
+                                    <InputGroup.Prepend >
+                                        <InputGroup.Text  >
+                                            {this.state.value === "" 
+                                                ? "Select user ..." 
+                                                : 
+                                                <div className = 'd-flex flex-row align-items-center'>
+                                                    <img 
+                                                        src ={users[this.state.value].avatar.avatarURL} 
+                                                        alt = {`Avatar of ${this.state.value}`}
+                                                        
+                                                        className = 'login-img mr-2'
+                                                        /> 
+                                                    <div>{this.state.value}</div>
+                                                </div>
+                                            }
+                                        </InputGroup.Text>
                                     
-                                <DropdownButton 
-                                    as = {InputGroup.Append}
-                                    id="dropdown-basic-button" 
-                                    title = ""
-                                    variant = 'outline-secondary'
-                                    onSelect = {(e) => this.handleSelect(e)}
-                                >
-                                    {Object.keys(users).map( user => (
-                                        <Dropdown.Item 
-                                            className = 'd-flex flex-row align-items-center'
-                                            key = {users[user].id}
-                                            eventKey= {users[user].id}
-                                            
+                                    </InputGroup.Prepend>
+                                        
+                                    <DropdownButton 
+                                        as = {InputGroup.Append}
+                                        id="dropdown-basic-button" 
+                                        title = ""
+                                        variant = 'outline-secondary'
+                                        onSelect = {(e) => this.handleSelect(e)}
+                                    >
+                                        {Object.keys(users).map( user => (
+                                            <Dropdown.Item 
+                                                className = 'd-flex flex-row align-items-center'
+                                                key = {users[user].id}
+                                                eventKey= {users[user].id}
+                                                
 
-                                        >
-                                            <img 
-                                                src = {users[user].avatar.avatarURL}
-                                                alt = {`Avatar of ${users[user].name}`}
-                                               
-                                                className = 'login-img mr-2'
-                                            />
-                                            <div>{users[user].name}</div>
-                                        </Dropdown.Item>
-                                    ))}
-                                </DropdownButton> 
+                                            >
+                                                <img 
+                                                    src = {users[user].avatar.avatarURL}
+                                                    alt = {`Avatar of ${users[user].name}`}
+                                                
+                                                    className = 'login-img mr-2'
+                                                />
+                                                <div>{users[user].name}</div>
+                                            </Dropdown.Item>
+                                        ))}
+                                    </DropdownButton> 
+                                    
+                                </InputGroup>
                                 
-                            </InputGroup>
+                                
                             
                             
-                           
-                        
-                            <button 
-                                className = 'btn btn-lg btn-primary mt-3'
-                                type = 'submit' 
-                                disabled = {this.state.value === ""}
-                            >
-                                Sign In
-                            </button>
-                        </div>
-                    </form>
-                </div>
-
+                                <button 
+                                    className = 'btn btn-lg btn-primary mt-3'
+                                    type = 'submit' 
+                                    disabled = {this.state.value === ""}
+                                >
+                                    Sign In
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>  
+                </div>                             
             </div>
             
         )
