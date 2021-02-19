f784e1001b5d2c5c33c3622d4dbc138df4a008e3
Taina Conde <tainabatistaconde@gmail.com>
2021-02-19 17:54:30 -0500

    style: Leader - score number

--- src/components/Leader.js
+++ src/components/Leader.js
@@ -21,19 +21,27 @@ function Leader(props) {
                         <div>
                             <h1>{user.name}</h1>
                         </div>
-                        <div className = 'border-bottom font-weight-bold d-flex justify-content-between'>
+                        <div className = 'border-bottom d-flex justify-content-between'>
                             <div>Answered questions: </div>
                             <div>{numQuestionsAnswered}</div>
                         </div>
-                        <div className = 'font-weight-bold d-flex justify-content-between' >
+                        <div className = 'd-flex justify-content-between' >
                             <div>Created questions:</div> 
                             <div>{numQuestionsAsked}</div>
                         </div>
                     </div>
                     
-                    <div className = 'col-sm-3'>
-                        <div>Score</div>
-                        <div>{score}</div>
+                    <div className = 'col-sm-3 d-flex flex-sm-column border p-0 score-box'>
+                        <div className = 'col-6 bg-light text-center'>Score</div>
+                        <div className = 'col-6 text-white d-flex p-0'>
+                            <div className = 'col p-3'> 
+                                <div 
+                                    className = 'col rounded-circle bg-info score-number d-flex align-items-center justify-content-center'
+                                >       {score}
+                                </div>
+                            </div>
+                            
+                        </div>
                     </div>
               
         </div>
